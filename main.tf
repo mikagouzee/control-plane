a37a7a3a7615dfd9c7569c63f7982eacd48381e0
mika <michel.gouzee@hotmail.com>
2025-02-11 16:14:47 +0100

    control plane and setup scripts

---
+++ main.tf
@@ -0,0 +1,175 @@
+resource "azurerm_resource_group" "rg" {
+  name = var.resource_group_name
+  location =  var.resource_group_location
+
+    tags = {
+        environment = "production"
+    }
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name = var.virtual_network_name
+  address_space = ["10.0.0.0/16"]
+  location = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  tags = {
+    environment = "production"
+  }
+}
+
+resource "azurerm_subnet" "subnet" {
+  name = var.subnet_name
+  resource_group_name = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes = ["10.0.1.0/24"]
+}
+
+resource "azurerm_public_ip" "public_ip" {
+  name = var.public_ip_name
+  location = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method = "Dynamic"
+  tags = {
+    environment = "production"
+  }
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  name = var.netwok_security_group_name
+  location = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name = "SSH"
+    priority = 1001
+    direction = "Inbound"
+    access = "Allow"
+    protocol = "Tcp"
+    source_port_range = "*"
+    destination_port_range = "22"
+    source_address_prefix = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "HTTP"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = {
+    environment = "production"
+  }
+}
+
+resource "azurerm_network_interface" "nic" {
+  name = var.network_interface_name
+  location = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name = "myNicConfig"
+    subnet_id = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.public_ip.id
+  }
+
+  tags = {
+    environment = "production"
+  }
+
+}
+
+resource "azurerm_network_interface_security_group_association" "association" {
+  network_interface_id = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+resource "random_id" "randomId"{
+    keepers = {
+        resource_group = azurerm_resource_group.rg.name
+    }
+    byte_length = 8
+}
+
+resource "azurerm_storage_account" "storage" {
+  name = "diag${random_id.randomId.hex}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location = azurerm_resource_group.rg.location
+  account_tier = "Standard"
+  account_replication_type = "LRS"
+  tags = {
+    environment = "production"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "linuxvm" {
+    name = var.linux_virtual_machine_name
+    location = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+    network_interface_ids = [azurerm_network_interface.nic.id]
+    size = "Standard_DS1_v2"
+    computer_name = "myvm"
+    admin_username = "azureuser"
+    disable_password_authentication = true
+    
+    os_disk {
+      name = "myOsDisk"
+      caching = "ReadWrite"
+      storage_account_type = "Premium_LRS"
+    }
+
+    source_image_reference {
+      publisher = "Canonical"
+      offer     = "0001-com-ubuntu-server-jammy"
+      sku       = "22_04-lts-gen2"
+      version   = "latest"
+    }   
+
+    admin_ssh_key {
+      username = "azureuser"
+      public_key = file("./keys/key.pub")
+    }
+
+    boot_diagnostics {
+      storage_account_uri = azurerm_storage_account.storage.primary_blob_endpoint
+    }
+
+    tags = {
+        environment = "production"
+    }
+
+    connection {
+      type     = "ssh"
+      user     = "azureuser"
+      private_key = file("./keys/key.pem")
+      host     = self.public_ip_address
+    }
+  
+    provisioner "file"{
+        source = "install-terraform.sh"
+        destination = "/tmp/install_terraform.sh"
+    }
+
+    provisioner "file" {
+        source = "setup.sh"
+        destination = "/tmp/setup.sh"
+    }
+
+    provisioner "remote-exec" {
+    inline = [
+        "chmod +x /tmp/install_terraform.sh",
+        "sudo /tmp/install_terraform.sh",
+        "chmod +x /tmp/setup.sh",
+        "sudo /tmp/setup.sh ${var.gitsource}"
+      ]
+    }
+}
+
