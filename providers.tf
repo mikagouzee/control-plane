7368bf33f0ff564a4d018d143a1e3157a62410db
mika <michel.gouzee@hotmail.com>
2025-03-11 11:31:27 +0100

    adapt cloud init and scripts

--- providers.tf
+++ providers.tf
@@ -1,15 +1,13 @@
 provider "azurerm" {
     features {}
 
-    use_msi = true
 }
 
 terraform {
     required_providers {
         azurerm = {
             source = "hashicorp/azurerm"
-            version = "2.78.0"
-            
+            version = "2.78.0"            
         }
     }
 }
